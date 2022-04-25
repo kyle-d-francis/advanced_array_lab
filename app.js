112fab6f2fe80d70af26b65f9bb553b269e05a11
kyle-d-francis <102608450+kyle-d-francis@users.noreply.github.com>
2022-04-25 11:12:44 -0500

    finished third problem
    successfully finished the third question

--- app.js
+++ app.js
@@ -146,10 +146,28 @@ console.log('Mexican Foods: ', mexicanFood)
 
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
-function italianProblem(){
+//function italianProblem(){
+
+    //let results = dishes.filter(function(el){
+        //if((el.cuisine === servings) &&(el.servings > 5)){
+            //return true;
+        //}
+        //else{
+            //return false;
+       // }});
+      
+
+
+    //return results;
+//} 
+//let results = italianProblem();
+//console.log('Italian food:' , results)
+//3. Create a function that will return only dishes whose serving id number matches their serving count.
+//Filter
+function servingProblem(){
 
     let results = dishes.filter(function(el){
-        if((el.cuisine === 'Italian') &&(el.servings > 5)){
+        if(el.id === el.servings){
             return true;
         }
         else{
@@ -160,10 +178,8 @@ function italianProblem(){
 
     return results;
 } 
-let results = italianProblem();
-console.log('Italian food:' , results)
-//3. Create a function that will return only dishes whose serving id number matches their serving count.
-//Filter
+let results = servingProblem();
+console.log('servings:' , results)
 
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
